e4a4bb766d79d9920e3eca34007d3df82c95b643
Maeyanie <maeyanie@deathwyrm.com>
2012-08-23 21:34:08 -0400

    Add GPL header, and rephrase coding style comment a bit.

--- MaeBackup.java
+++ MaeBackup.java
@@ -1,9 +1,23 @@
 /*
- * Before people complain, yes, I know this isn't the "proper" Java coding style.
- * I started writing it in C then switched part way through, and kept with the C style.
- * But it works. And it's one file instead of a gazillion. So bite me. :p
- *  - Mae
- */
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Before people complain, yes, I know this isn't the "proper" Java coding style.
+It was originally written in C, but I switched it using Glacier. 
+Amazon  doesn't offer a C library, so I adapted it to Java, and kept the C coding style.
+But it works. And it's one file instead of a gazillion. So bite me. :p
+ - Mae
+*/
 
 package maebackup;
 
